c89ef95d8404e620124cf510942ca5e57d5b38e9
Harnoor Singh <hsingh9@student.gsu.edu>
2023-09-12 00:36:09 -0400

    Added Glide to set images in Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/week2android/MainActivity.kt
+++ app/src/main/java/com/example/week2android/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.week2android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.example.week2android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+
+        _binding.button.setOnClickListener {
+            // Way 1 to set Image
+//            val newImageDrawable: Drawable? = getDrawable( R.drawable.image1)
+            val newImageDrawable: Drawable? = ContextCompat.getDrawable(this, R.drawable.image1)
+            _binding.imageView.setImageDrawable(newImageDrawable)
+
+            setImageUsingGlide()
+        }
+    }
+
+    private fun setImageUsingGlide(){
+        Glide
+            .with(this)
+            .load("https://image.tmdb.org/t/p/original/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg")
+            .centerCrop()
+            .into(_binding.imageView);
     }
 }
