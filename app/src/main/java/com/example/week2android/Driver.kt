8d040fe946f05e717d4301a1360271621c4b87af
Harnoor Singh <hsingh9@student.gsu.edu>
2023-09-25 11:51:08 -0400

    added notes from class

--- app/src/main/java/com/example/week2android/Driver.kt
+++ app/src/main/java/com/example/week2android/Driver.kt
@@ -10,17 +10,39 @@ object Driver {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        // optionals concept
-        val first: String? = null
-        println(first!!.length)
+        """Concept of Optionals / nullables"""
+        val str: String = "Hello"
+//        print(str)
+        val empl: Employee? = null // ? = object can be null
+//        val empl: Employee? = Employee(1,"Harry") // ? = object can be null
+        // compile time error // grammar
+        //print(empl.employeeId)
 
+        // option 1
+//        if (empl == null) {
+//            print("empl is null")
+//        } else {
+//            print(empl.employeeId)
+//        }
+
+        // option 4 // no null condition
+        empl?.let {
+            print(empl.employeeId)
+        }
+        // option2
+//        print(empl?.employeeId) // ? print null if null otherwise value
+
+        // option 3
+//        print(empl!!.employeeId) //i guarantee that it is never null
+        // so force unwrap
+
+        //Concept of Lamdas
         // Immutables vs Mutables
-        val result = performOperation(10, 5) {
-                a, b -> a - b
+        val result = performOperation(10, 5) { a, b ->
+            a - b
         }
         println("Result of subtraction: $result") // Output: Result of
     }
-
 
     fun performOperation(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
         return operation(a, b)
---
+++ app/src/main/java/com/example/week2android/Employee.kt
@@ -0,0 +1,4 @@
+package com.example.week2android
+
+class Employee(val employeeId: Int, val employeeName:String) {
+}
